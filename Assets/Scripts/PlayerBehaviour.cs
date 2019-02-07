c6335da678522d9b9402e110132b6cb1472b2e6f
yesrolya <yesrolya@yandex.ru>
2019-02-08 01:37:08 +1000

    initial commit comment

---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public float rotationSpeed = 100.0f;
+    [SerializeField] private GameObject panel;
+    [SerializeField] private GameObject textWin;
+    private bool menu = false;
+    
+    void Update()
+    {
+        //Escape button
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            if (!menu)
+            {
+
+            }
+            else
+            {
+
+            }
+            menu = !menu;
+        }
+    }
+
+    //physics
+    void FixedUpdate()
+    {
+        if (!menu)
+        {
+            //movement
+            float translation = Input.GetAxis("Vertical") * speed;
+            float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+
+            translation *= Time.deltaTime;
+            rotation *= Time.deltaTime;
+
+            transform.Translate(0, 0, translation);
+            transform.Rotate(0, rotation, 0);
+        }
+    }
+}
